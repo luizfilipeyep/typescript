fc31292b7cf2f46309ad892b02ebb757e1e9b63c
Luiz Omena <83364189+luizfilipeyep@users.noreply.github.com>
2026-05-08 04:44:52 -0300

    classes

---
+++ classes.ts
@@ -0,0 +1,18 @@
+// Classes
+class Person {
+  id: number;
+  name: string;
+  age: number;
+
+  constructor(id: number, name: string, age: number) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+  }
+
+  sayMyName(): string {
+    return this.name;
+  }
+}
+
+const person = new Person(1, "Felipe", 21);
